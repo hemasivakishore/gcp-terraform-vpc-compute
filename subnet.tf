ef2515877c8a9f51105316524526462bbcc3c435
hemasivakishore <hemasivakishore@gmail.com>
2025-12-18 00:09:21 +0530

    Add subnet and region variables for Google Cloud subnets

---
+++ subnet.tf
@@ -0,0 +1,27 @@
+resource "google_compute_subnetwork" "main-subnet-1" {
+  name          = "main-subnet-1"
+  ip_cidr_range = var.subnet-cidr-1
+  region        = var.region-subnet-1
+  network       = google_compute_network.vpc-main.name
+}
+
+resource "google_compute_subnetwork" "main-subnet-2" {
+  name          = "main-subnet-2"
+  ip_cidr_range = var.subnet_cidr-2
+  region        = var.region-subnet-2
+  network       = google_compute_network.vpc-main.name
+}
+
+resource "google_compute_subnetwork" "main-subnet-3" {
+  name          = "main-subnet-3"
+  ip_cidr_range = var.subnet-cidr-3
+  region        = var.region-subnet-3
+  network       = google_compute_network.vpc-main.name
+}
+
+resource "google_compute_subnetwork" "main-subnet-4" {
+  name          = "main-subnet-4"
+  ip_cidr_range = var.subnet-cidr-4
+  region        = var.region-subnet-4
+  network       = google_compute_network.vpc-main.name
+}
---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "subnet-cidr-1" {
+  description = "The CIDR range for subnet 1"
+  type        = string
+}
+
+variable "region-subnet-1" {
+  description = "The region for subnet 1"
+  type        = string
+}
+
+variable "subnet_cidr-2" {
+  description = "The CIDR range for subnet 2"
+  type        = string
+}
+
+variable "region-subnet-2" {
+  description = "The region for subnet 2"
+  type        = string
+}
+
+variable "subnet-cidr-3" {
+  description = "The CIDR range for subnet 3"
+  type        = string
+}
+
+variable "region-subnet-3" {
+  description = "The region for subnet 3"
+  type        = string
+}
+
+variable "subnet-cidr-4" {
+  description = "The CIDR range for subnet 4"
+  type        = string
+}
+
+variable "region-subnet-4" {
+  description = "The region for subnet 4"
+  type        = string
+}
