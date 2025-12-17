a7738e1da1641ed2374ca0d6e09f645b4da38d85
hemasivakishore <hemasivakishore@gmail.com>
2025-12-18 01:40:48 +0530

    Refactor subnet region variables for consistency and clarity

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
+variable "region" {
+  description = "The region where resources will be created"
+  type        = string
+
+}
 variable "subnet-cidr-1" {
   description = "The CIDR range for subnet 1"
-  type        = string
-}
-
-variable "region-subnet-1" {
-  description = "The region for subnet 1"
   type        = string
 }
 
@@ -13,28 +13,13 @@ variable "subnet_cidr-2" {
   type        = string
 }
 
-variable "region-subnet-2" {
-  description = "The region for subnet 2"
-  type        = string
-}
-
 variable "subnet-cidr-3" {
   description = "The CIDR range for subnet 3"
   type        = string
 }
 
-variable "region-subnet-3" {
-  description = "The region for subnet 3"
-  type        = string
-}
-
 variable "subnet-cidr-4" {
   description = "The CIDR range for subnet 4"
-  type        = string
-}
-
-variable "region-subnet-4" {
-  description = "The region for subnet 4"
   type        = string
 }
 
