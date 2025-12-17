ba9b0dcc8807b6c893ad8dd3f04ed21ce8397300
Hema Siva Kishore <70573794+hemasivakishore@users.noreply.github.com>
2025-12-17 23:24:28 +0530

    Add Terraform provider configuration for Google Cloud

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "7.14.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-1e2da3fc-bb97-4b70-9c0"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
